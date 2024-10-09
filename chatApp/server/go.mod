8440dc32ce41fc4fdba42e3e6ef153ce8c170fb9
Rishav Mehra <rishavmehra61@gmail.com>
2024-10-09 15:13:37 +0530

    feat: webSocket connection created

--- chatApp/server/go.mod
+++ chatApp/server/go.mod
@@ -1,3 +1,5 @@
 module github.com/rishavmehra/chatapp
 
 go 1.22.1
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- chatApp/server/main.go
+++ chatApp/server/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println(string(p)) // need to convert into string
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func serverWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Sever")
+	})
+
+	http.HandleFunc("/ws", serverWs)
+}
 
 func main() {
 	fmt.Println("Hello from chat APP")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }

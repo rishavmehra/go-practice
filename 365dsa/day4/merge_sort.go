fdcff08a8a9b607a07da8ecf1ecad76ebefe1d4f
rishavmehra <rishavmehra61@gmail.com>
2025-01-08 00:04:43 +0530

    day4: merge sort

---
+++ 365dsa/day4/merge_sort.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{23, 1, 10, 5, 2}
+	res := MergeSort(arr)
+	fmt.Println(res)
+
+}
+
+func MergeSort(items []int) []int {
+	if len(items) < 2 {
+		return items
+	}
+
+	first := MergeSort(items[:len(items)/2])
+	second := MergeSort(items[len(items)/2:])
+
+	return merge(first, second)
+
+}
+
+func merge(a []int, b []int) []int {
+	final := []int{}
+	i := 0
+	j := 0
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			final = append(final, a[i])
+			i++
+		} else {
+			final = append(final, b[j])
+			j++
+		}
+	}
+
+	for ; i < len(a); i++ {
+		final = append(final, a[i])
+	}
+	for ; j < len(b); j++ {
+		final = append(final, b[j])
+	}
+	return final
+}
